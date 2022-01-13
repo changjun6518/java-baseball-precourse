72b5ebd75aeef80e17024f8a6689baa7142a7393
최창준 <aowert6518@gmail.com>
2022-01-13 21:38:29 +0900

    [refactor] test code 클래스 변경

---
+++ src/test/java/baseball/ComputerTest.java
@@ -0,0 +1,34 @@
+package baseball;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ComputerTest {
+
+    @Test
+    public void generateComputerNumber() throws Exception {
+        // given
+        Computer computer = new Computer();
+        computer.generateNumber();
+
+        // when
+        String computerNumber = computer.getNumber();
+
+
+        // then
+        System.out.println("computerNumber = " + computerNumber);
+        Assertions.assertEquals(computerNumber.length(), 3);
+    }
+
+    @Test
+    public void generateRandomNumber() throws Exception {
+        // given
+        // when
+        String randomNumber = RandomNumberFactory.generate();
+
+        // then
+        System.out.println("randomNumber = " + randomNumber);
+        Assertions.assertEquals(randomNumber.length(), 3);
+        Assertions.assertTrue(Integer.parseInt(randomNumber) < 1000);
+    }
+}
